c24197c0d87f9ce6b2e0d11be3d3398bde60766d
timenbob <timen.bobnar@gmail.com>
2023-12-03 19:48:40 +0100

    neki

--- DN1/druga.m
+++ DN1/druga.m
@@ -10,7 +10,7 @@ w=[-1;-1];
 %prva naloga
 x1=diskrVeriznica(w,obesisceL,obesisceD,L,M);
 [n,m]=size(x1);
-odg1=mean(x1(1,:));
+odg1=mean(x1(1,:))
 
 %druga
 %za wp rabimo M G in H mase so kar M G=g in h
@@ -19,7 +19,7 @@ h=zeros(1, size(M,2));
 for i = 1:size(M,2)
     h(i)=(y(i)+y(i+1))/2;
 end
-odg2=M*h'*g;
+odg2=M*h'*g
 
 %tretja
 
--- DN1/prva.m
+++ DN1/prva.m
@@ -19,7 +19,7 @@ n1=10000;
 
 [y1,v1,t1] = padalec(parametri1,zac1,tk1,n1);
 index_25s=find(t1>=25,1);
-odg1=zac1(1,1)-y1(index_25s);
+odg1=zac1(1,1)-y1(index_25s)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 format long
 parametri2=[90,1,0.9];
@@ -35,14 +35,14 @@ tk22=60;
 n22=10000;
 [y22,v22,t22] = padalec(parametri22,zac22,tk22,n22);
 
-odg2=abs(v22(end,1)-v2(end,1));
+odg2=abs(v22(end,1)-v2(end,1))
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 parametri3=[90,1,0.9];
 zac3=[40000;0];
 tk3=1000;
 n3=10000;
 [y3,v3,t3] = padalec(parametri3,zac3,tk3,n3);
-odg3=min(v3);
+odg3=min(v3)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 parametri4=[90,1,0.9];
 zac4=[40000;0];
